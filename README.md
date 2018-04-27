40937d85c26f5cdc163f048e6e8588591e202933
aaronpenne <aaronpenne@users.noreply.github.com>
2018-04-27 15:13:34 -0700

    Added executive order text files

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# government_corpus
-A collection of government texts and scrapers to get them, starting with US Presidential Executive Orders.
+# Government Corpus and Scrapers
+A collection of government texts and scrapers to get them.
+
+## UCSB American Presidency Project
+
+The best collection I could find for documents relating to the USA Presidency are [hosted by the UCSB American Presidency Project (APP)](http://www.presidency.ucsb.edu/index.php). They have a great [search tool](http://www.presidency.ucsb.edu/index.php#search), but I could not find a way to bulk download the documents to text files, thus the scraper.
+
+Description from the APP site as of 2018-04-27:
+
+>The American Presidency Project (APP), non-profit and non-partisan, is the leading source of presidential documents on the internet. Our archives contain 129,483 documents and are growing rapidly.  
+The APP, hosted at the University of California, Santa Barbara, has been a collaboration between John T. Woolley (UCSB) and Gerhard Peters (Citrus College) since 1999.
+
+The site includes the full text of executive orders, presidential debates, and much more:
+
+- Messages and Papers of the Presidents
+- Public Papers of the Presidents
+- Public Papers and Addresses of Franklin D. Roosevelt
+- Weekly Compilation of Presidential Documents
+- Daily Compilation of Presidential Documents
+- Annual Messages to Congress on the State of the Union
+- Inaugural Addresses
+- Farewell Addresses
+- The President's Weekly Addresses
+- Fireside Chats
+- News Conferences
+- Executive Orders
+- Proclamations
+- Presidential Signing Statements
+- The Economic Report of the President
+- Statements of Administration Policy
+- White House Media Pool Reports
+- Remarks by the Vice President
+- Remarks and Statements by the First Lady
+- Presidential Nomination Acceptance Speeches and Addresses
+- Presidential Candidates Debates
+- Documents Related to Presidential Elections
+- Documents Related to the 2000 Election Dispute
+- Documents Related to Presidential Transitions
+- National Political Party Platforms
---
+++ scraper_usa_president.py
@@ -0,0 +1,150 @@
+"""
+Scraper for US Presidential documents from http://www.presidency.ucsb.edu/index_docs.php
+
+Author: Aaron Penne
+
+Created: 2018-04-28
+"""
+
+import os
+import pandas as pd
+from datetime import datetime
+import requests
+from bs4 import BeautifulSoup
+
+def scrape(url):
+    result = requests.get(url)
+    if result.status_code != 200:
+        print('ERROR: Scrape status code {}'.format(result.status_code))
+        return ''
+    return BeautifulSoup(result.content, 'lxml')
+
+def set_output_dir(dir_name):
+    # Set output directory, make it if needed
+    output_dir = os.path.join('output')
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
+    output_dir = os.path.join(output_dir, dir_name)
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
+    return output_dir
+
+# -----------------------------------------------------------------------------
+# Executive Orders
+# -----------------------------------------------------------------------------
+soup = scrape('http://www.presidency.ucsb.edu/executive_orders.php')
+output_dir = set_output_dir('executive_orders')
+
+# Get all years available from drop down menu
+# I think this more readable than list comprehension
+years_available = []
+for node in soup.find_all('option'):
+    year = node.find_all(text=True)[0]
+    years_available.append(year)
+
+# Get metadata for each executive order from year page
+metadata = {'pres': [],
+            'date': [],
+            'title': [],
+            'link': []}
+print('Getting metadata')
+for year in years_available:
+    soup = scrape('http://www.presidency.ucsb.edu/executive_orders.php?year={}&Submit=DISPLAY'.format(year))
+    table = soup.select('table')[10]
+    for i, row in enumerate(table.select('tr')):
+        # Skip header
+        if i == 0:
+            continue
+        cell = row.select('td')
+        metadata['pres'].append(''.join(c[0] for c in cell[0].text.split()))  # Get abbreviated name ()
+        date = cell[1].text.split()
+        year = date[2]
+        month = date[0]
+        day = date[1][:-1]
+        if int(date[1][:-1]) < 1 or int(date[1][:-1]) > 31:
+            print('Fixing date: {}'.format(cell[1].text)) 
+            day = '1'
+        date = '{} {} {}'.format(month, day, year)
+        metadata['date'].append(datetime.strptime(date, '%B %d %Y').strftime('%Y%m%d'))
+        metadata['title'].append(cell[2].text)
+        metadata['link'].append('http://www.presidency.ucsb.edu{}'.format(cell[2].a.get('href')[2:]))
+        
+print('Saving metadata')
+df = pd.DataFrame(metadata, columns=['date', 'pres', 'link', 'exec'])
+df.index.name = 'index'
+df.to_csv('list_executive_orders.csv')
+
+# Pull down all the docs
+print('Getting executive orders')
+for i, date in enumerate(metadata['date']):
+    if i % 10 == 0:
+        print('{:0.2f}%'.format(100*i/len(metadata['date'])))
+    date = metadata['date'][i]
+    pres = metadata['pres'][i]
+    output_file = os.path.join(output_dir, '{}_Executive_Order_{}.txt'.format(date, pres))
+    if not os.path.isfile(output_file):
+        soup = scrape(metadata['link'][i])
+        text = soup.find('span', class_='displaytext')
+        text = text.get_text('\n')
+        with open(output_file, 'w+') as f:
+            f.write(text)
+
+# -----------------------------------------------------------------------------     
+# Proclamations
+# -----------------------------------------------------------------------------
+soup = scrape('http://www.presidency.ucsb.edu/proclamations.php')
+output_dir = set_output_dir('proclamations')
+
+# Get all years available from drop down menu
+years_available = []
+for node in soup.find_all('option'):
+    year = node.find_all(text=True)[0]
+    years_available.append(year)
+    
+    
+# Get metadata for each doc from year page
+metadata = {'pres': [],
+            'date': [],
+            'title': [],
+            'link': []}
+print('Getting metadata')
+for year in years_available:
+    soup = scrape('http://www.presidency.ucsb.edu/proclamations.php?year={}&Submit=DISPLAY'.format(year))
+    table = soup.select('table')[10]
+    for i, row in enumerate(table.select('tr')):
+        # Skip header
+        if i == 0:
+            continue
+        cell = row.select('td')
+        metadata['pres'].append(''.join(c[0] for c in cell[0].text.split()))  # Get abbreviated name ()
+        date = cell[1].text.split()
+        year = date[2]
+        month = date[0]
+        day = date[1][:-1]
+        if int(date[1][:-1]) < 1 or int(date[1][:-1]) > 31:
+            print('Fixing date: {}'.format(cell[1].text)) 
+            day = '1'
+        date = '{} {} {}'.format(month, day, year)
+        metadata['date'].append(datetime.strptime(date, '%B %d %Y').strftime('%Y%m%d'))
+        metadata['title'].append(cell[2].text)
+        metadata['link'].append('http://www.presidency.ucsb.edu{}'.format(cell[2].a.get('href')[2:]))
+        
+print('Saving metadata')
+df = pd.DataFrame(metadata, columns=['date', 'pres', 'link', 'exec'])
+df.index.name = 'index'
+df.to_csv('list_proclamations.csv')
+
+# Pull down all the docs
+print('Getting proclamations')
+for i, date in enumerate(metadata['date']):
+    if i % 10 == 0:
+        print('{:0.2f}%'.format(100*i/len(metadata['date'])))
+    date = metadata['date'][i]
+    pres = metadata['pres'][i]
+    output_file = os.path.join(output_dir, '{}_Proclamations_{}.txt'.format(date, pres))
+    if not os.path.isfile(output_file):
+        soup = scrape(metadata['link'][i])
+        text = soup.find('span', class_='displaytext')
+        text = text.get_text('\n')
+        with open(output_file, 'w+') as f:
+            f.write(text)
